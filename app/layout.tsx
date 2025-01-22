f3c7f71f940d907936f780d100e1b4946a91628b
v.popova <v.popova@blubito.com>
2025-01-22 21:57:53 +0200

    added authentication layer with Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
+
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,14 @@ import Container from './Container';
 import MobileMenu from './MobileMenu';
 import SearchBar from './SearchBar';
 import CartItem from './CartItem';
+import { currentUser } from '@clerk/nextjs/server';
+import { ClerkLoaded, SignedIn, SignInButton, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import { ListOrdered } from 'lucide-react';
 
-const Header = () => {
+const Header = async () => {
+  const user = await currentUser();
+  console.log(user);
   return (
     <header className='border-b border-b-gray-400 py-5'>
       <Container className='flex items-center justify-between gap-7 text-lightColor'>
@@ -20,9 +26,20 @@ const Header = () => {
         <div className='w-auto md:w-1/3 flex items-center justify-end gap-5'>
           <SearchBar />
           <CartItem />
-          <div>
-            <button className='text-sm font-semibold hover:text-darkColor hoverEffect'>Login</button>
-          </div>
+          <ClerkLoaded>
+            <SignedIn>
+              <Link href={'/orders'} className='group relative'>
+                <ListOrdered className='w-5 h-5 group-hover:text-darkColor hoverEffect' />
+                <span className='absolute -top-1 -right-1 bg-darkColor text-white h-3.5 w-3.5 rounded-full tetx-xs font-semibold flex items-center justify-center'>0</span>
+              </Link>
+              <UserButton />
+            </SignedIn>
+            {!user && (
+              <SignInButton mode='modal'>
+                <button className='text-sm font-semibold hover:text-darkColor hoverEffect'>Login</button>
+              </SignInButton>
+            )}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>

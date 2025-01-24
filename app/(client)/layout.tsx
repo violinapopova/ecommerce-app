bfdce18d47abd36dc0c4c85fbd8a38ad4bc9049f
v.popova <violipopova@gmail.com>
2025-01-24 23:26:06 +0200

    added the backend layer with Sanity

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import localFont from "next/font/local";
 
 const raleway = localFont({
-  src: "./fonts/Raleway.woff2",
+  src: "../fonts/Raleway.woff2",
   variable: "--font-raleway",
   weight: "100 900",
 });
---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+import { categoryType } from './categoryType'
+import { productType } from './productType'
+import { orderType } from './orderType'
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [categoryType, productType, orderType],
+}

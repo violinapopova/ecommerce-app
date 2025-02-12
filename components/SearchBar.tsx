353c58cb5e7579f33efd4ea1f1d602f68f13f908
v.popova <violipopova@gmail.com>
2025-02-12 21:18:35 +0200

    changes to SearchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,67 @@
-import { Search } from 'lucide-react';
-import React from 'react';
+import { Search, X } from 'lucide-react';
+import React, { useCallback, useState } from 'react';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+import { Input } from './ui/input';
+import { client } from '@/sanity/lib/client';
 
 const SearchBar = () => {
+  const [search, setSearch] = useState('');
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+
+  const fetchProducts = useCallback(async () => {
+    if (!search) {
+      setProducts([]);
+      return;
+    }
+    setLoading(true);
+    try {
+      const query = `*[_type == "product" && name match $search] | order(name asc)`;
+      const params = { search: `${search}*` };
+      const response = await client.fetch(query, params);
+      setProducts(response);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [search]);
+
   return (
-    <div>
-        <Search className='w-5 h-5 hover:text-darkColor hoverEffect'/>
-    </div>
+    <Dialog open={showSearch} onOpenChange={() => setShowSearch(!showSearch)}>
+      <DialogTrigger onClick={() => setShowSearch(!showSearch)}>
+        <Search className="w-5 h-5 hover:text-darkColor hoverEffect" />
+      </DialogTrigger>
+      <DialogContent className="max-w-5xl min-h-[90vh] max-h-[90vh] flex flex-col overflow-hidden">
+        <DialogHeader>
+          <DialogTitle className="mb-1">Product Searchbar</DialogTitle>
+          <form className="relative" onSubmit={(e) => e.preventDefault()}>
+            <Input
+              placeholder="Search your product here..."
+              className="flex-1 rounded-md py-5"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            {search && (
+              <X
+                onClick={() => setSearch("")}
+                className="w-4 h-4 absolute top-3 right-11 hover:text-red-600 hoverEffect"
+              />
+            )}
+            <button
+              type="submit"
+              className={`absolute right-0 top-0 w-10 h-full flex items-center justify-center rounded-tr-md rounded-br-md hover:bg-darkColor hover:text-white hoverEffect ${search ? "bg-darkColor text-white" : "bg-darkColor/10"}`}
+            >
+              <Search className="w-5 h-5" />
+            </button>
+          </form>
+        </DialogHeader>
+        <div className="w-full h-full overflow-y-scroll border border-darkColor/20 rounded-md">
+          <p>Searching on progress...</p>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
 

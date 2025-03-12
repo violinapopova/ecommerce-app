df35838d4874d874127e1ff9ba2cd99afe3d8a92
v.popova <violipopova@gmail.com>
2025-03-12 21:26:40 +0200

    created the orders page

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,29 +1,71 @@
-import CategoryProducts from "@/components/CategoryProducts";
+
 import Container from "@/components/Container";
+import OrdersComponent from "@/components/OrdersComponent";
 import Title from "@/components/Title";
-import { getAllCategories } from "@/sanity/helpers/queries";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { getMyOrders } from "@/sanity/helpers/queries";
+import { auth } from "@clerk/nextjs/server";
+import { FileX } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const CategoryPage = async ({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) => {
-  const { slug } = await params;
-  const categories = await getAllCategories();
-  console.log('slug:', slug);
+const OrdersPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const orders = await getMyOrders(userId);
 
   return (
     <Container className="py-10">
-      <Title className="text-xl">
-        Products by Category 
-        {/* <span className="font-bold text-green-600 capitalize tracking-wide">
-          {slug && slug}
-        </span> */}
-      </Title>
-      <CategoryProducts categories={categories} slug={slug} />
+      {orders?.length ? (
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="text-2xl md:text-3xl">Order List</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ScrollArea className="w-full">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-auto">Order Number</TableHead>
+                    <TableHead className="hidden md:table-cell">Date</TableHead>
+                    <TableHead>Customer</TableHead>
+                    <TableHead className="hidden sm:table-cell">
+                      Email
+                    </TableHead>
+                    <TableHead>Total</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead className="hidden sm:table-cell">
+                      Invoice Number
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <OrdersComponent orders={orders} />
+                <ScrollBar orientation="horizontal" />
+              </Table>
+            </ScrollArea>
+          </CardContent>
+        </Card>
+      ) : (
+        <div className="flex flex-col items-center justify-center py-5 md:py-10 px-4">
+          <FileX className="h-24 w-24 text-gray-400 mb-4" />
+          <Title>No orders found</Title>
+          <p className="mt-2 text-sm text-gray-600 text-center max-w-md">
+            It looks like you haven&apos;t placed any orders yet. Start shopping
+            to see your orders here!
+          </p>
+          <Button asChild className="mt-6">
+            <Link href={"/"}>Browse Products</Link>
+          </Button>
+        </div>
+      )}
     </Container>
   );
 };
 
-export default CategoryPage;
+export default OrdersPage;
